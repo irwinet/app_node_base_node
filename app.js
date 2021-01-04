87bfbee395fd46d1c6a901960a68b910c3f30109
Irwin <irwinestradatorres@gmail.com>
2021-01-04 17:25:45 -0500

    Configure Yargs

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar.js');
 
 // let base = '3';
@@ -6,12 +20,15 @@ const { crearArchivo } = require('./multiplicar/multiplicar.js');
 // console.log(multiplicar);
 // console.log(process.argv);
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
+
+console.log('Limite', argv.limite);
+// console.log(argv2);
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
 
 // console.log(base);
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(err => console.log(err));
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo creado: ${archivo}`))
+//     .catch(err => console.log(err));
