44639caf45b23539353220e4f3bc342f689017c1
Irwin <irwinestradatorres@gmail.com>
2021-01-04 16:54:06 -0500

    Add Parameter for Command Inline

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const { crearArchivo } = require('./multiplicar/multiplicar.js');
 
-let base = '3';
+// let base = '3';
 
 // console.log(module);
 // console.log(multiplicar);
+// console.log(process.argv);
+
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1];
+
+// console.log(base);
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
