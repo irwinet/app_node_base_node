6d067c8e5c16d8d1ffe8e67960705b08fcdb51d1
Irwin <irwinestradatorres@gmail.com>
2021-01-04 16:07:56 -0500

    Add File System

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-let base = 2;
+// requireds
+const fs = require('fs');
+// const fs = require('express'); No son nativos de Node
+// const fs = require('./fs');
+
+let base = 5;
+let data = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} * ${i} = ${base * i}`);
+    // console.log(`${base} * ${i} = ${base * i}`);
+    data += `${base} * ${i} = ${base * i}\n`;
 }
+
+
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`El archivo tabla-${base}.txt ha sido creado!`);
+});
