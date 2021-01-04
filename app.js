b78b3a4c4c2a47530fed3169f1e66168132879c3
Irwin <irwinestradatorres@gmail.com>
2021-01-04 16:39:01 -0500

    Add Exports Module

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-// requireds
-const fs = require('fs');
-// const fs = require('express'); No son nativos de Node
-// const fs = require('./fs');
+const { crearArchivo } = require('./multiplicar/multiplicar.js');
 
-let base = 5;
-let data = '';
+let base = '3';
 
-for (let i = 1; i <= 10; i++) {
-    // console.log(`${base} * ${i} = ${base * i}`);
-    data += `${base} * ${i} = ${base * i}\n`;
-}
+// console.log(module);
+// console.log(multiplicar);
 
-
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-    if (err) throw err;
-    console.log(`El archivo tabla-${base}.txt ha sido creado!`);
-});
+crearArchivo(base)
+    .then(archivo => console.log(`Archivo creado: ${archivo}`))
+    .catch(err => console.log(err));
---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,32 @@
+// requireds
+const fs = require('fs');
+// const fs = require('express'); No son nativos de Node
+// const fs = require('./fs');
+
+let crearArchivo = (base) => {
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un numero`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= 10; i++) {
+            // console.log(`${base} * ${i} = ${base * i}`);
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) reject(err);
+            else
+                resolve(`tabla-${base}.txt`);
+            // console.log(`El archivo tabla-${base}.txt ha sido creado!`);
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo
+}
